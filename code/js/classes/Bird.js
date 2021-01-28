479fc0e13e527ccaa33d8ffb1f76f993cbafdc63
Jadounix <jademariepetit@yahoo.fr>
2021-01-28 21:45:46 +0100

    création d'une fonction permettant de faire le lien entre les cases de la grille et les pixels

--- code/js/classes/Bird.js
+++ code/js/classes/Bird.js
@@ -20,16 +20,37 @@ class Bird {
     this.positionY = newPosition;
   }
   //Récupération de l'image à afficher et affichage la première fois dans le canvas
-  CreateBirdPicture(ctx)
+  CreateBirdPicture(ctx,x,y)
   {
     this.birdPicture.src = this.pictureSource;
     this.birdPicture.onload = () => {
-    ctx.drawImage(this.birdPicture, this.startPositionX, this.startPositionY);
+    ctx.drawImage(this.birdPicture, x, y);
     }
   }
   //Dessin de l'oiseau une fois que l'image est déjà crée. x et y représente les coodonées de position
   DrawBirdPicture(ctx,x,y)
   {
     ctx.drawImage(this.birdPicture, x, y);
+  }
+
+  //Comportement de déplacement de l'oiseau
+  MoveBehavior(nbCell, step)
+  {
+    //Ddécrire plusieurs comportements en fonction du type d'oiseau
+    if (this.positionX < nbCell - 1) {
+      this.SetXPosition(this.positionX + step);
+    }
+
+    if (this.positionY < nbCell - 1) {
+      //this.SetYPosition(this.positionY + step);
+    }
+
+    if (this.positionX > 0) {
+      //this.SetYPosition(this.positionX - step);
+    }
+
+    if (this.positionY > 0) {
+      this.SetYPosition(this.positionY - step);
+    }
   }
 }
