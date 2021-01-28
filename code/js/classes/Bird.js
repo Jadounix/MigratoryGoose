7a8c474754711e40e8fe51eef239baf04171923f
Jadounix <jademariepetit@yahoo.fr>
2021-01-28 19:27:10 +0100

    Ajout du deuxième oiseau + classe Bird

--- code/js/classes/Bird.js
+++ code/js/classes/Bird.js
@@ -1,12 +1,35 @@
-class Bird
-{
+class Bird {
   //Constructeur
-  constructor(type)
+  constructor(species,startPositionX, startPositionY, positionX, positionY, pictureSource, birdPicture)
   {
-    //Type de l'oiseau : migrateur ou non
-    this.type = type;
+    this.species = species;
+    this.startPositionX = startPositionX;
+    this.startPositionY = startPositionY;
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.pictureSource = pictureSource;
+    this.birdPicture = new Image();
   }
-
   //Méthodes
-
+  SetXPosition(newPosition)
+  {
+    this.positionX = newPosition;
+  }
+  SetYPosition(newPosition)
+  {
+    this.positionY = newPosition;
+  }
+  //Récupération de l'image à afficher et affichage la première fois dans le canvas
+  CreateBirdPicture(ctx)
+  {
+    this.birdPicture.src = this.pictureSource;
+    this.birdPicture.onload = () => {
+    ctx.drawImage(this.birdPicture, this.startPositionX, this.startPositionY);
+    }
+  }
+  //Dessin de l'oiseau une fois que l'image est déjà crée. x et y représente les coodonées de position
+  DrawBirdPicture(ctx,x,y)
+  {
+    ctx.drawImage(this.birdPicture, x, y);
+  }
 }
