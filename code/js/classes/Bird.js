cfcf96f56609e89a93b224d3547f8084c4934d8a
Jadounix <jademariepetit@yahoo.fr>
2021-01-30 15:05:36 +0100

    Création de la classe map
    Ajout d'une matrice qui doit se superposer à la grille du canvas pour avoir des valeurs sur la grille. Le lien entre les deux n'est pas encore fait

--- code/js/classes/Bird.js
+++ code/js/classes/Bird.js
@@ -1,7 +1,6 @@
 class Bird {
   //Constructeur
-  constructor(species,startPositionX, startPositionY, positionX, positionY, pictureSource, birdPicture)
-  {
+  constructor(species, startPositionX, startPositionY, positionX, positionY, pictureSource, birdPicture) {
     this.species = species;
     this.startPositionX = startPositionX;
     this.startPositionY = startPositionY;
@@ -11,46 +10,71 @@ class Bird {
     this.birdPicture = new Image();
   }
   //Méthodes
-  SetXPosition(newPosition)
-  {
+  SetXPosition(newPosition) {
     this.positionX = newPosition;
   }
-  SetYPosition(newPosition)
-  {
+  SetYPosition(newPosition) {
     this.positionY = newPosition;
   }
   //Récupération de l'image à afficher et affichage la première fois dans le canvas
-  CreateBirdPicture(ctx,x,y)
-  {
+  CreateBirdPicture(ctx, x, y) {
     this.birdPicture.src = this.pictureSource;
     this.birdPicture.onload = () => {
-    ctx.drawImage(this.birdPicture, x, y);
+      ctx.drawImage(this.birdPicture, x, y);
     }
   }
   //Dessin de l'oiseau une fois que l'image est déjà crée. x et y représente les coodonées de position
-  DrawBirdPicture(ctx,x,y)
-  {
+  DrawBirdPicture(ctx, x, y) {
     ctx.drawImage(this.birdPicture, x, y);
   }
 
+  RandomMove(nbCell, step) {
+    let rdn = Math.floor(Math.random() * 4); //Retourne un nombre aléatoire entre 0 et 3
+    //let rdn = 2;
+    switch (rdn) {
+      case 0: //Déplacement vers le haut
+        if (this.positionY > 0) {
+          this.SetYPosition(this.positionY - step);
+        }
+        break;
+      case 1: //Déplacement vers la droite
+        if (this.positionX < nbCell - 1) {
+          this.SetXPosition(this.positionX + step);
+        }
+        break;
+      case 2: //Déplacement vers la gauche
+        if (this.positionX > 0) {
+          this.SetXPosition(this.positionX - step);
+        }
+        break;
+      case 3: //Déplacement vers le bas
+        if (this.positionY < nbCell - 1) {
+          this.SetYPosition(this.positionY + step);
+        }
+        break;
+      default:
+        console.log('No move');
+    }
+    rdn = Math.floor(Math.random() * 3);
+  }
+
   //Comportement de déplacement de l'oiseau
-  MoveBehavior(nbCell, step)
-  {
-    //Ddécrire plusieurs comportements en fonction du type d'oiseau
-    if (this.positionX < nbCell - 1) {
-      this.SetXPosition(this.positionX + step);
-    }
+  MoveBehavior(nbCell, step) {
+    //Déplacement différent en fonction de l'espèce de l'oiseau
+    switch (this.species) {
+      case 'bleu':
+        this.RandomMove(nbCell, step);
+        break;
 
-    if (this.positionY < nbCell - 1) {
-      //this.SetYPosition(this.positionY + step);
-    }
+      case 'rouge':
+        this.RandomMove(nbCell, step);
+        break;
 
-    if (this.positionX > 0) {
-      //this.SetYPosition(this.positionX - step);
-    }
-
-    if (this.positionY > 0) {
-      this.SetYPosition(this.positionY - step);
+      default:
+        console.log('error : Unknown bird species');
     }
   }
-}
+
+
+
+} //Fin de la classe
--- code/js/classes/Map.js
+++ code/js/classes/Map.js
@@ -1,12 +1,51 @@
 class Map
 {
   //Constructeur
-  constructor(size)
+  constructor(padding, dimensionX, dimensionY, cellSize, cellNumber, matrice)
   {
     //Taille de la map
-    this.size = size;
+    this.padding = padding;
+    this.dimensionX = dimensionX;
+    this.dimensionY = dimensionY;
+    this.cellSize = cellSize;
+    this.cellNumber = cellNumber;
+    //Tableau qui va contenir les valeurs des cases de la map
+    this.matrice = new Array(this.dimensionX/this.cellSize);
+    for (let i = 0; i < this.matrice.length; i++) {
+      this.matrice[i] = new Array(this.dimensionY/this.cellSize);}
+  }
+  //Méthodes
+  //Dessin de la grille sur le canvas
+  drawBoard(ctx) {
+    for (let x = 0; x <= this.dimensionX; x += this.cellSize) {
+      ctx.moveTo(0.5 + x + this.padding, this.padding);
+      ctx.lineTo(0.5 + x + this.padding, this.dimensionY + this.padding);
+    }
+    for (let x = 0; x <= this.dimensionY; x += this.cellSize) {
+      ctx.moveTo(this.padding, 0.5 + x + this.padding);
+      ctx.lineTo(this.dimensionX + this.padding, 0.5 + x + this.padding);
+    }
+    ctx.strokeStyle = "black";
+    ctx.stroke();
   }
 
-  //Méthodes
+  //Initialisation de la map avec des valeurs false ie des cases qui ne sont occupées par rien
+  mapInitialisation()
+  {
+    for(let i = 0; i<this.dimensionx/this.cellSize; i++)
+		{
+			for(let j=0;i<this.dimensionY/this.cellSize; j++)
+      {
+        this.matrice[i][j] = false;
+      }
+		}
+  }
 
+  //Dessin d'une case bonus sur la map
+  DrawBonusCell(ctx,x,y)
+  {
+    let greenSquareImage = new Image();
+    greenSquareImage.src = 'images/green_square.png';
+    ctx.drawImage(greenSquareImage, x, y);
+  }
 }
